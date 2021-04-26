e090aca118960bdaada3af8cd1fd2d007ba0b959
Jose Cuentas <jcuentas@intercorp.com.pe>
2021-04-25 20:09:56 -0500

    chore(config): add firebase analytics

--- app/src/main/java/com/example/firebaselearning/presentation/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/firebaselearning/presentation/feature/main/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.firebaselearning.presentation.feature.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firebaselearning.R
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = Firebase.analytics
     }
 }
